c1f94aefc1d59fc3b9afb4a71e4691a87b5472f7
Ashan <ashanvimodh241@gmail.com>
2025-05-26 01:11:25 +0530

    Add Repository.cs 1.02

--- Models/IRepository.cs
+++ Models/IRepository.cs
@@ -2,7 +2,7 @@ using System.Linq.Expressions;
 
 namespace Mr_and_Mrs_DineIn.Models
 {
-    public interface IRepository<in T> where T : class
+    public interface IRepository<T> where T : class
     {
         Task<IEnumerable<T>> GetAllAsync();
         Task<T> GetByIdAsync(int id, QueryOptions<T> options);
