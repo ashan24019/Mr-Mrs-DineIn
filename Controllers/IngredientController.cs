2c7d92642aa853490a608f21e14792ee6bc906ae
Ashan <ashanvimodh241@gmail.com>
2025-05-28 13:52:39 +0530

    Add delete

--- Controllers/IngredientController.cs
+++ Controllers/IngredientController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Identity.Client;
 using Mr_and_Mrs_DineIn.Data;
 using Mr_and_Mrs_DineIn.Models;
 
@@ -20,14 +21,49 @@ namespace Mr_and_Mrs_DineIn.Controllers
         public async Task<IActionResult> Details(int id)
         {
             return View(await ingredients.GetByIdAsync(id, new QueryOptions<Ingredient>() { Includes = "ProductIngredients.Product" }));
-
-
-            // Ingredient?create
-            [HttpGet]
-
-            public IActionResult Create()
-            {
-                return View();
-            }
         }
+
+
+        // Ingredient?create
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("IngredientId, Name")] Ingredient ingredient)
+        {
+            if (ModelState.IsValid)
+            {
+                await ingredients.AddAsync(ingredient);
+                return RedirectToAction(nameof(Index));
+            }
+            return View(ingredient);
+        }
+
+        // Ingredient?Delete
+        [HttpGet]
+
+        public async Task<IActionResult> Delete(int id)
+        {
+            return View(await ingredients.GetByIdAsync(id, new QueryOptions<Ingredient> { Includes = "ProductIngredients.Product"}));
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+
+        public async Task<IActionResult> Delete(Ingredient ingredient)
+        {
+          
+            await ingredients.DeleteAsync(ingredient.IngredientId);
+            return RedirectToAction("Index");
+           
+        }
+        [HttpGet]
+        public async Task<IActionResult> Edit(int id)
+        {
+            return View(await ingredients.GetByIdAsync(id, new QueryOptions<Ingredient> { Includes = "ProductIngredients.Product"}));
+        }
+    }
 }
